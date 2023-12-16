1be86b5848b618d5c3342f401e1c67b2ef9cec7e
Nikhil <nikhilpal2017@gmail.com>
2023-12-17 01:06:21 +0530

    Installation of client using create-react-app

---
+++ client/src/App.js
@@ -0,0 +1,23 @@
+import {
+  BrowserRouter, Navigate, Routes, Route 
+} from 'react-router-dom'
+import HomePage from "pages/HomePage";
+import LoginPage from "pages/LoginPage";
+import ProfilePage from 'pages/ProfilePage';
+
+
+function App() {
+  return (
+    <div className="app">
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<LoginPage/>} />
+          <Route path='/home' element={<HomePage/>} />
+          <Route path='/profile/:userId' element={<ProfilePage/>} />
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
+}
+
+export default App;
